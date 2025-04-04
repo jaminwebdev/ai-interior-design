498dea20046ac4a388ed9f709a48e5e82aed0f10
Jamin <jaminwebdev@gmail.com>
2025-04-04 15:17:05 -0500

    Header and Footer to dash layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,16 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="min-h-screen">{children}</div>;
+  return (
+    <div className="min-h-screen">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 }
