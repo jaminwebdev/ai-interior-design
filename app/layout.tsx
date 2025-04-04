c7900d30bbf8627f6f52dcbb862e66c3f9ef2bca
Jamin <jaminwebdev@gmail.com>
2025-04-04 15:30:12 -0500

    header and theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} px-5`}>
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ThemeToggle } from './ThemeToggle';
+import { shadow } from '@/lib/styles';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header
+      className="flex justify-between items-center py-5 px-5"
+      style={{
+        boxShadow: shadow,
+      }}
+    >
+      <h1>Logo</h1>
+      <ThemeToggle />
+    </header>
+  );
 }
 
 export default Header;
